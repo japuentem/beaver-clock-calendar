de1e0fc778df43a16d06f9037e3249d1f93be2f4
alberto puente <japuentem@gmail.com>
2023-04-17 12:16:52 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'beaver-clock-calendar',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
